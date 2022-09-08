91ed36ad1f5eac03aa6e7c7a209a64f4c74a25e3
Palak Mittal <66310688+palak2104@users.noreply.github.com>
2022-09-09 04:39:07 +0530

    Create K-th element of two sorted Arrays.cpp

---
+++ Binary Search/K-th element of two sorted Arrays.cpp
@@ -0,0 +1,22 @@
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+      if(n+m<k) return 0; 
+      int low = max(0,k-m), high = min(k,n);
+      while(low<=high){
+      int cut1=(low+high)/2;
+      int cut2=k-cut1;
+      int l1=cut1==0?INT_MIN:arr1[cut1-1];
+      int l2=cut2==0?INT_MIN:arr2[cut2-1];
+      int r1=cut1==n?INT_MAX:arr1[cut1];
+      int r2=cut2==m?INT_MAX:arr2[cut2];
+      if(l1<=r2 && l2<=r1){
+      return max(l1,l2);
+      }
+      if(l1>r2) high=cut1-1;
+      else low=cut1+1;
+      }
+      return 1;
+    }
+};
